799db659ea93f3985081018740aac47b57898984
Josh Harcombe <joshharc@gmail.com>
2022-01-16 23:10:03 +1100

    add dumb bf implementation

---
+++ src/main.rs
@@ -0,0 +1,88 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} <file>", args[0]);
+        return;
+    }
+
+    let code = fs::read_to_string(&args[1]).expect("Cannot read file");
+    run_code(&code);
+}
+
+fn run_code(code: &str) {
+    let mut cells: Vec<u8> = vec![0; 30000];
+    let mut ptr = 0;
+    let mut pc = 0;
+
+    while pc < code.len() {
+        let c = code.chars().nth(pc).unwrap();
+
+        match c {
+            '>' => {
+                ptr += 1;
+                if ptr >= cells.len() {
+                    cells.push(0);
+                }
+            }
+            '<' => {
+                ptr -= 1;
+            }
+            '+' => {
+                cells[ptr] = cells[ptr].checked_add(1).unwrap_or(0);
+            }
+            '-' => {
+                cells[ptr] = cells[ptr].checked_sub(1).unwrap_or(u8::MAX);
+            }
+            '.' => {
+                print!("{}", cells[ptr] as char);
+            }
+            ',' => {
+                // TODO: input is kinda cooked, find a buffered solution
+                // currently input is restricted to one char per line
+
+                let mut input = String::new();
+                std::io::stdin().read_line(&mut input).unwrap();
+                if let Some(c) = input.as_bytes().first() {
+                    cells[ptr] = *c as u8;
+                }
+            }
+            '[' => {
+                if cells[ptr] == 0 {
+                    let mut depth = 1;
+                    while depth > 0 {
+                        if pc >= code.len() {
+                            panic!("Parsing error: unmatched [");
+                        }
+                        pc += 1;
+                        match code.chars().nth(pc).unwrap() {
+                            '[' => depth += 1,
+                            ']' => depth -= 1,
+                            _ => {}
+                        }
+                    }
+                }
+            }
+            ']' => {
+                if cells[ptr] != 0 {
+                    let mut depth = 1;
+                    while depth > 0 {
+                        if pc == 0 {
+                            panic!("Parsing error: unmatched ]");
+                        }
+                        pc -= 1;
+                        match code.chars().nth(pc).unwrap() {
+                            ']' => depth += 1,
+                            '[' => depth -= 1,
+                            _ => {}
+                        }
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        pc += 1;
+    }
+}
