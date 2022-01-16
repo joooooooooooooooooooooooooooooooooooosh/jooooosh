34755a560d872f917825615e333165b831259913
Josh Harcombe <joshharc@gmail.com>
2022-01-17 00:30:30 +1100

    adapt bf logic for jo+sh

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 use std::{env, fs};
 
+const POINTER_RIGHT: usize = 1;
+const POINTER_LEFT: usize = 2;
+const INCREMENT: usize = 3;
+const DECREMENT: usize = 4;
+const PRINT_CHAR: usize = 5;
+const INPUT_CHAR: usize = 6;
+const START_LOOP: usize = 7;
+const END_LOOP: usize = 8;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -19,30 +28,35 @@ fn run_code(code: &str) {
     let mut cells: Vec<u8> = vec![0; 30000];
     let mut ptr = 0;
     let mut pc = 0;
+    let mut lines = code.lines();
+
+    lines.find(|line| line.contains("j"))
+         .expect("Invalid program - no j found");
+    let code = lines.collect::<Vec<&str>>();
 
     while pc < code.len() {
-        let c = code.chars().nth(pc).unwrap();
+        let line = code[pc];
 
-        match c {
-            '>' => {
+        match line.chars().filter(|c| *c == 'o').count() {
+            POINTER_RIGHT => {
                 ptr += 1;
                 if ptr >= cells.len() {
                     cells.push(0);
                 }
             }
-            '<' => {
+            POINTER_LEFT => {
                 ptr -= 1;
             }
-            '+' => {
+            INCREMENT => {
                 cells[ptr] = cells[ptr].checked_add(1).unwrap_or(0);
             }
-            '-' => {
+            DECREMENT => {
                 cells[ptr] = cells[ptr].checked_sub(1).unwrap_or(u8::MAX);
             }
-            '.' => {
+            PRINT_CHAR => {
                 print!("{}", cells[ptr] as char);
             }
-            ',' => {
+            INPUT_CHAR => {
                 // TODO: input is kinda cooked, find a buffered solution
                 // currently input is restricted to one char per line
 
@@ -52,7 +66,7 @@ fn run_code(code: &str) {
                     cells[ptr] = *c as u8;
                 }
             }
-            '[' => {
+            START_LOOP => {
                 if cells[ptr] == 0 {
                     let mut depth = 1;
                     while depth > 0 {
@@ -60,15 +74,16 @@ fn run_code(code: &str) {
                             panic!("Parsing error: unmatched [");
                         }
                         pc += 1;
-                        match code.chars().nth(pc).unwrap() {
-                            '[' => depth += 1,
-                            ']' => depth -= 1,
+                        match line.chars().filter(|c| *c == 'o').count() {
+                            // TODO: broken logic
+                            START_LOOP => depth += 1,
+                            END_LOOP => depth -= 1,
                             _ => {}
                         }
                     }
                 }
             }
-            ']' => {
+            END_LOOP => {
                 if cells[ptr] != 0 {
                     let mut depth = 1;
                     while depth > 0 {
@@ -76,15 +91,18 @@ fn run_code(code: &str) {
                             panic!("Parsing error: unmatched ]");
                         }
                         pc -= 1;
-                        match code.chars().nth(pc).unwrap() {
-                            ']' => depth += 1,
-                            '[' => depth -= 1,
+                        match line.chars().filter(|c| *c == 'o').count() {
+                            START_LOOP => depth += 1,
+                            END_LOOP => depth -= 1,
                             _ => {}
                         }
                     }
                 }
             }
             _ => {}
+        }
+        if line.contains("sh") {
+            break;
         }
 
         pc += 1;
@@ -95,6 +113,7 @@ fn transpile_from_bf(code: &str) {
     let mut pc = 0;
 
     print!("j");
+
     while pc < code.len() {
         let c = code.chars().nth(pc).unwrap();
 
